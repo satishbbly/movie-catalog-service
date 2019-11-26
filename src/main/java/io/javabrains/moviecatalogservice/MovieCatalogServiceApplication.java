a575476740a1c4dd96a8c4b500795a7571f85c9a
Satish Kumar <satishbbly@gmail.com>
2019-11-26 21:12:01 +0530

    some modification

--- src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,5 +1,6 @@
 package io.javabrains.moviecatalogservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
